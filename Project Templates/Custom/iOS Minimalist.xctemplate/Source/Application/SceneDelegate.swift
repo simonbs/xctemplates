66a37e605e62e8b017976fd6ecd102f8c108923a
Simon Støvring <mail@simonbs.dk>
2021-09-12 08:28:25 +0200

    Adds fatal error

--- Project Templates/Custom/iOS Minimalist.xctemplate/Source/Application/SceneDelegate.swift
+++ Project Templates/Custom/iOS Minimalist.xctemplate/Source/Application/SceneDelegate.swift
@@ -13,7 +13,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = scene as? UIWindowScene else {
-            return
+            fatalError("Expected scene of type UIWindowScene but got an unexpected type")
         }
         window = UIWindow(windowScene: windowScene)
         window?.rootViewController = UINavigationController(rootViewController: MainViewController())
